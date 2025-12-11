745c1af9357074d353d73f592e37919fed7ec85e
devid-suarez <davidsuarezlosada@gmail.com>
2025-12-11 01:49:34 -0500

    Fix GitHub Pages base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
+    base: '/gamimg/',
     plugins: [angular({ tsconfig: './tsconfig.json' })],
     server: {
         host: '0.0.0.0',
